ce7fa0a9926af27fbcb4e38ea47967f53a050152
ArtemOrlovUA <Artemorel200415@gmail.com>
2023-04-05 19:23:09 +0300

    v2 (add date, limit of notes)

--- script.js
+++ script.js
@@ -1,39 +1,56 @@
-notes = [];
+let notes = [];
+
+function addLeadingZero(d) {
+  return d < 10 ? '0' + d : d;
+}
+
+function getNoteTime(t) {
+  let Y = t.getFullYear();
+  let M = addLeadingZero(t.getMonth() + 1);
+  let D = addLeadingZero(t.getDate());
+
+  return '' + D + '.' + M + '.' + Y;
+}
 
 const addMovie = (ev) => {
   ev.preventDefault();
-  let note = {
-    date: Date.now(),
-    name: document.getElementById('noteName').value,
-    discription: document.getElementById('noteDisc').value,
-    category: document.getElementById('noteCat').value,
-  };
 
-  notes.push(note);
+  if (notes.length == 10) {
+    alert('You added maximum notes!');
+  } else {
+    let note = {
+      date: getNoteTime(new Date(Date.now())),
+      name: document.getElementById('noteName').value,
+      discription: document.getElementById('noteDisc').value,
+      category: document.getElementById('noteCat').value,
+    };
 
-  tbody = document.getElementById('tbodyActive');
+    notes.push(note);
 
-  let tr = document.createElement('tr');
+    tbody = document.getElementById('tbodyActive');
 
-  tbody.appendChild(tr);
+    let tr = document.createElement('tr');
 
-  let tdName = document.createElement('td');
-  tr.appendChild(tdName).innerText = note.name;
+    tbody.appendChild(tr);
 
-  let tdCreated = document.createElement('td');
-  tr.appendChild(tdCreated).innerText = note.date;
+    let tdName = document.createElement('td');
+    tr.appendChild(tdName).innerText = note.name;
 
-  let tdCatg = document.createElement('td');
-  tr.appendChild(tdCatg).innerText = note.category;
+    let tdCreated = document.createElement('td');
+    tr.appendChild(tdCreated).innerText = note.date;
 
-  let tdDisc = document.createElement('td');
-  tr.appendChild(tdDisc).innerText = note.discription;
+    let tdCatg = document.createElement('td');
+    tr.appendChild(tdCatg).innerText = note.category;
 
-  let tdDates = document.createElement('td');
-  tr.appendChild(tdDates);
+    let tdDisc = document.createElement('td');
+    tr.appendChild(tdDisc).innerText = note.discription;
 
-  let tdActions = document.createElement('td');
-  tr.appendChild(tdActions);
+    let tdDates = document.createElement('td');
+    tr.appendChild(tdDates);
+
+    let tdActions = document.createElement('td');
+    tr.appendChild(tdActions);
+  }
 
   document.forms[0].reset();
 };
